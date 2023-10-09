3633cc385cd351711743032c89d2344cd90d20af
ennuo <ajr.uiz2048@gmail.com>
2023-10-09 03:58:06 -0400

    Implement netcode hash algorithm

--- BombServer/Src/Helpers/BombHMAC.cs
+++ BombServer/Src/Helpers/BombHMAC.cs
@@ -9,42 +9,26 @@ namespace BombServerEmu_MNR.Src.Helpers
 {
     class BombHMAC
     {
-        public static ushort GetMD516(byte[] data) //TODO: Salt
+        public static ushort GetHMACMD5(byte[] data, int salt)
         {
-            switch (data[0])
+            var outer = new byte[4 + data.Length];
+            var inner = new byte[20];
+
+            for (var i = 0; i < 4; ++i)
             {
-                case 0x66:
-                    Buffer.BlockCopy(new byte[2], 0, data, 12, 2);
-                    break;
-                case 0x65:
-                    Buffer.BlockCopy(new byte[2], 0, data, 6, 2);
-                    break;
-                case 0x62:
-                    Buffer.BlockCopy(new byte[2], 0, data, 2, 2);
-                    break;
+                var b = (byte)(salt >> ((3 - i) * 8));
+                outer[i] = (byte)(b ^ 0x5c);
+                inner[i] = (byte)(b ^ 0x36);
             }
 
-            var hmac = HMACMD5.Create().ComputeHash(data);
-
+            Buffer.BlockCopy(data, 0, outer, 4, data.Length);
+            var hash = MD5.Create().ComputeHash(outer);
+            Buffer.BlockCopy(hash, 0, inner, 4, hash.Length);
+            hash = MD5.Create().ComputeHash(inner);
+            
             ushort result = 0;
             for (int i=0; i<16; i+=2) //Squish down our HMAC into 16 bits
-                result ^= (ushort)((hmac[i + 1] << 8) ^ hmac[i]);
-            return result;
-        }
-        public static uint GetMD532(byte[] data) //TODO: Salt
-        {
-            switch (data[0])
-            {
-                case 0x64:
-                    Buffer.BlockCopy(new byte[4], 0, data, 8, 4);
-                    break;
-            }
-
-            var hmac = HMACMD5.Create().ComputeHash(data);
-
-            uint result = 0;
-            for (int i = 0; i < 16; i += 4) //Squish down our HMAC into 32 bits
-                result ^= (ushort)(hmac[i] ^ (hmac[i + 1] << 8) ^ (hmac[i + 2] << 16) ^ (hmac[i + 3] << 24));
+                result ^= (ushort)((hash[i + 1] << 8) ^ hash[i]);
             return result;
         }
     }
