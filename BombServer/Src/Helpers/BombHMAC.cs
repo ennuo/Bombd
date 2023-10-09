9faf6f329e4c0bb1f3e71bf7c50f9cd22ace207d
ennuo <ajr.uiz2048@gmail.com>
2023-10-09 05:21:48 -0400

    Add HMAC32

--- BombServer/Src/Helpers/BombHMAC.cs
+++ BombServer/Src/Helpers/BombHMAC.cs
@@ -9,7 +9,7 @@ namespace BombServerEmu_MNR.Src.Helpers
 {
     class BombHMAC
     {
-        public static ushort GetHMACMD5(byte[] data, int salt)
+        private static byte[] GetHMAC(byte[] data, uint salt)
         {
             var outer = new byte[4 + data.Length];
             var inner = new byte[20];
@@ -24,11 +24,25 @@ namespace BombServerEmu_MNR.Src.Helpers
             Buffer.BlockCopy(data, 0, outer, 4, data.Length);
             var hash = MD5.Create().ComputeHash(outer);
             Buffer.BlockCopy(hash, 0, inner, 4, hash.Length);
-            hash = MD5.Create().ComputeHash(inner);
             
+            return MD5.Create().ComputeHash(inner);
+        }
+        
+        public static ushort GetMD516(byte[] data, uint salt)
+        {
+            var hmac = GetHMAC(data, salt);
             ushort result = 0;
-            for (int i=0; i<16; i+=2) //Squish down our HMAC into 16 bits
-                result ^= (ushort)((hash[i + 1] << 8) ^ hash[i]);
+            for (var i = 0; i < 16; i += 2) //Squish down our HMAC into 16 bits
+                result ^= (ushort)((hmac[i + 1] << 8) ^ hmac[i]);
+            return result;
+        }
+        
+        public static uint GetMD532(byte[] data, uint salt)
+        {
+            var hmac = GetHMAC(data, salt);
+            uint result = 0;
+            for (var i = 0; i < 16; i += 4) //Squish down our HMAC into 32 bits
+                result ^= (uint)(hmac[i] ^ (hmac[i + 1] << 8) ^ (hmac[i + 2] << 16) ^ (hmac[i + 3] << 24));
             return result;
         }
     }
