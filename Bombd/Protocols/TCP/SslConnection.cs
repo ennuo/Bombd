96bc50f907d42901e5ad8b1e69fda314e0a4bb2b
derole <38840902+derole1@users.noreply.github.com>
2025-10-17 18:01:42 +0100

    Fix stale sockets when a client uncleanly disconnects from an SSL service

--- Bombd/Protocols/TCP/SslConnection.cs
+++ Bombd/Protocols/TCP/SslConnection.cs
@@ -13,7 +13,9 @@ public class SslConnection : ConnectionBase
 {
     private const int MaxMessageSize = 8192;
     private const int KeepAliveFrequency = 3000;
-    
+    private const int ReadTimeout = 10_000;
+    private const int WriteTimeout = 10_000;
+
     // u32 MsgLength
     // char Md5Digest[16]
     // char Protocol
@@ -47,12 +49,12 @@ public class SslConnection : ConnectionBase
         try
         {
             _sslStream = new SslStream(new NetworkStream(_socket, false), false);
-            
+
             // Prevent lingering connections, the game should periodically send keep alive packets
             // in response to our own.
-            _sslStream.ReadTimeout = 10_000;
-            _sslStream.WriteTimeout = 10_000;
-            
+            _sslStream.ReadTimeout = ReadTimeout;
+            _sslStream.WriteTimeout = WriteTimeout;
+
             await _sslStream.AuthenticateAsServerAsync(_server.Certificate, false, SslProtocols.Ssl3, false);
             
             _keepAliveTimer = new Timer(KeepAliveFrequency);
@@ -66,7 +68,7 @@ public class SslConnection : ConnectionBase
             return;
         }
         
-        DoBlockAndReceive();
+        DoBlockAndReceive(ReadTimeout);
     }
 
     public override void Disconnect()
@@ -157,7 +159,7 @@ public class SslConnection : ConnectionBase
         }
     }
 
-    private async void DoBlockAndReceive()
+    private async void DoBlockAndReceive(int readTimeout)
     {
         while (State != ConnectionState.Disconnected)
         {
@@ -165,20 +167,23 @@ public class SslConnection : ConnectionBase
             PacketType type;
             try
             {
-                int len = await _sslStream.ReadAsync(_recv.AsMemory(0, MessageHeaderSize));
-                
-                // Socket has been shutdown on the other side
-                if (len == 0)
+                using (var cTokenSrc = CreateCancellationTokenTimeout(readTimeout))
                 {
-                    Disconnect();
-                    return;
-                }
-                
-                if (len != MessageHeaderSize)
-                {
-                    Logger.LogError<SslConnection>("Received message with invalid header. Closing connection.");
-                    Disconnect();
-                    return;
+                    int len = await _sslStream.ReadAsync(_recv.AsMemory(0, MessageHeaderSize), cTokenSrc.Token);
+
+                    // Socket has been shutdown on the other side
+                    if (len == 0)
+                    {
+                        Disconnect();
+                        return;
+                    }
+
+                    if (len != MessageHeaderSize)
+                    {
+                        Logger.LogError<SslConnection>("Received message with invalid header. Closing connection.");
+                        Disconnect();
+                        return;
+                    }
                 }
 
                 payloadSize = ((_recv[0] << 24) | (_recv[1] << 16) | (_recv[2] << 8) | _recv[3]) -
@@ -196,14 +201,18 @@ public class SslConnection : ConnectionBase
                     int offset = 0;
                     do
                     {
-                        len = await _sslStream.ReadAsync(_recv.AsMemory(offset, payloadSize - offset));
-                        if (len == 0)
+                        using (var cTokenSrc = CreateCancellationTokenTimeout(readTimeout))
                         {
-                            Disconnect();
-                            return;
-                        }
+                            int len = await _sslStream.ReadAsync(_recv.AsMemory(offset, payloadSize - offset), cTokenSrc.Token);
 
-                        offset += len;
+                            if (len == 0)
+                            {
+                                Disconnect();
+                                return;
+                            }
+
+                            offset += len;
+                        }
                     } while (offset < payloadSize);
                 }
             }
@@ -223,5 +232,12 @@ public class SslConnection : ConnectionBase
                 else Service.OnData(this, data, type);
             }   
         }
+    }
+
+    private CancellationTokenSource CreateCancellationTokenTimeout(int timeout)
+    {
+        var cTokenSrc = new CancellationTokenSource();
+        cTokenSrc.CancelAfter(timeout);
+        return cTokenSrc;
     }
 }
